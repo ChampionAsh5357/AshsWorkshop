0d7a8586fea8513ebcc923290fdbc5491be92e32
ChampionAsh5357 <ash@ashwork.net>
2024-08-18 23:41:00 -0400

    feat(docs): Box layer docs

--- src/main/java/net/ashwork/mc/ashsworkshop/client/sudoku/SudokuBoxLayer.java
+++ src/main/java/net/ashwork/mc/ashsworkshop/client/sudoku/SudokuBoxLayer.java
@@ -10,17 +10,35 @@ package net.ashwork.mc.ashsworkshop.client.sudoku;
  */
 public enum SudokuBoxLayer {
     // White background and transparent layer color (color marking)
+    /**
+     * The background color of the box. May be changed due to markings
+     * or invalid values.
+     */
     BACKGROUND,
-    // If the main value is invalid
-    INVALID_MAIN_VALUE_BACKGROUND,
     // Aesthetics (constraints as well)
+    /**
+     * The aesthetics of the board, from constraints to visual changes.
+     */
     AESTHETICS,
     // Fog of war
+    /**
+     * A fog that covers the background. Any necessary tints and invalid values
+     * are also applied here.
+     */
     FOG_OF_WAR,
     // Selection
+    /**
+     * Whether an individual box is focused or not.
+     */
     SELECTION,
     // Main value
+    /**
+     * The main value assumed for the sudoku box.
+     */
     MAIN_VALUE,
     // Markings if main value isn't set
+    /**
+     * Markings if no main value is set for the box.
+     */
     MARKINGS
 }
